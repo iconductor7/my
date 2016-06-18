3f5d41e6bdb6f3c8351f7bdf65c48e56ed787793
iconductor7 <iconductor7@gmail.com>
2016-06-18 21:56:23 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,4 @@
-
-ffmpeg -i input_video.mp4 -ss 00:00:30 -t 00:00:15 -c:v copy cropped.mp4
+ffmpeg -i input_video.mp4 -ss 00:00:30 -t 00:00:10 -c:v copy cropped.mp4
 convert -background transparent -depth 8 -resize 50% plate.svg plate.png
-ffmpeg -i cropped.mp4 -i plate.png -filter_complex "overlay=0:main_h-130:enable='between(t,1,14)'" -c:a copy plated.mp4
-ffmpeg -i plated.mp4 -vf drawtext="enable='between(t,2,14)':fontfile=Arial.ttf:text='I am learning computer graphics': fontcolor=black:fontsize=23:x=200:y=476" result.mp4
+ffmpeg -i cropped.mp4 -i plate.png -filter_complex "overlay=0:main_h-130:enable='between(t,1,9)'" -c:a copy plated.mp4
+ffmpeg -i plated.mp4 -vf drawtext="enable='between(t,2,9)':fontfile=Arial.ttf:text='I am learning computer graphics': fontcolor=black:fontsize=23:x=200:y=476" result.mp4
